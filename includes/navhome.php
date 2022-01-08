2810f0831cde41a750ec02c0bddcfae33e047c2a
H4CK3R5 <h4ck3r5pc@gmail.com>
2022-01-08 16:58:28 +0100

    push

---
+++ includes/navhome.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    
+    <a class="nav-link active" aria-current="page" id="font" href="../otherPages/new.php">new article</a>
+    
+  </div>
+</nav>
--- otherPages/home.php
+++ otherPages/home.php
@@ -1,3 +1,11 @@
 <?php
     require('../action/security.php');
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php  include('../includes/head.php');  include('../includes/navhome.php');    ?>
+<body>
+    
+</body>
+</html>
