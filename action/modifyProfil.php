3646ae28dd68048010f78a993d979ecfbccb2f43
H4CK3R5 <h4ck3r5pc@gmail.com>
2022-01-22 14:41:13 +0100

    push

--- action/modifyProfil.php
+++ action/modifyProfil.php
@@ -2,9 +2,10 @@
     require('db.php');
 
     if (isset($_POST['validate'])) {
-        if (!empty($_POST['mail']) AND !empty($_POST['username'])) {
+        if (!empty($_POST['mail']) AND !empty($_POST['username']) AND !empty($_POST['password'])) {
             $mail = $_POST['mail'];
             $username = $_POST['username'];
+            $pass = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
             $cheki = $bdd->prepare('SELECT email FROM users WHERE email = ?');
             $cheker = $bdd->prepare('SELECT username FROM users WHERE username = ?');
@@ -16,8 +17,8 @@
                 $mailinfo = $cheki->fetch();
                 $userInfo = $cheker->fetch();
 
-                $update = $bdd->prepare('UPDATE users SET email = ?, username = ? WHERE id = ?');
-                $update->execute(array($mail, $username, $_SESSION['ID']));
+                $update = $bdd->prepare('UPDATE users SET email = ?, username = ?, mdp = ? WHERE id = ?');
+                $update->execute(array($mail, $username, $pass, $_SESSION['ID']));
 
                 header('Location: ../otherPages/home.php');
             } else {
--- otherPages/modify.php
+++ otherPages/modify.php
@@ -19,6 +19,10 @@
             <label for="exampleInputPassword1" class="form-label">email</label>
             <input type="mail" class="form-control" name="mail" placeholder="<?= $_SESSION['mail'];?>">
         </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">password</label>
+            <input type="password" class="form-control" name="password" placeholder="new password">
+        </div>
                 
         <button type="submit" class="btn btn-primary" name="validate">modify</button>
         <br><br>
