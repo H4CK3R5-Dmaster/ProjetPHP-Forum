6a0ffeea1c11497802b9ae592b7d33f7b1bf4e17
H4CK3R5 <h4ck3r5pc@gmail.com>
2022-01-22 12:03:04 +0100

    push

---
+++ baseforum.sql
@@ -0,0 +1,121 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : sam. 22 jan. 2022 à 11:36
+-- Version du serveur : 10.4.22-MariaDB
+-- Version de PHP : 8.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `baseforum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `answer`
+--
+
+CREATE TABLE `answer` (
+  `id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `id_article` int(11) NOT NULL,
+  `contenu` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `titre` text NOT NULL,
+  `description` text NOT NULL,
+  `contenu` text NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `user_username` varchar(255) NOT NULL,
+  `date_publish` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `mdp` text NOT NULL,
+  `email` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`ID`, `username`, `mdp`, `email`) VALUES
+(1, 'Admin', '$2y$10$F8.IkZnU0QHvWHnz6Ys/lO6LBphtokmoACOfERyXKF7VrSrBi2v6e', 'admin@admin.com');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `answer`
+--
+ALTER TABLE `answer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `answer`
+--
+ALTER TABLE `answer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -4,9 +4,42 @@
         <link rel="icon" href="../assets/img/logo.png" type="image/x-icon">
     </head>
     <?php include('./includes/navbar.php'); include('./includes/head.php');?>
-    <?php require('./assets/style.php');?>
+    <?php require('./assets/style2.php');?>
 <body>
+    <svg viewBox="0 0 1800 600">
+    <symbol id="s-text">
+        <text text-anchor="middle"
+            x="50%"
+            y="35%"
+            class="webcoderskull"
+            >
+        Forum
+        </text>
+        <text text-anchor="middle"
+            x="50%"
+            y="68%"
+            class="text--line"
+            >
+        TRY IT
+        </text>
+        
+    </symbol>
     
+    <g class="g-ants">
+        <use xlink:href="#s-text"
+        class="webcoderskull-1"></use>     
+        <use xlink:href="#s-text"
+        class="webcoderskull-1"></use>     
+        <use xlink:href="#s-text"
+        class="webcoderskull-1"></use>     
+        <use xlink:href="#s-text"
+        class="webcoderskull-1"></use>     
+        <use xlink:href="#s-text"
+        class="webcoderskull-1"></use>     
+    </g>
+    
+    
+    </svg>
 </body>
 </html>
 
