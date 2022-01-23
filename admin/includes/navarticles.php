f2ecd315df0561be5d4d558a660d3f955c43f428
H4CK3R5 <h4ck3r5pc@gmail.com>
2022-01-23 16:56:00 +0100

    push

--- admin/includes/navarticles.php
+++ admin/includes/navarticles.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
   <div class="container-fluid">
     <a class="navbar-brand btn-outline-success btn" href="../otherPages/home.php">Retour</a>
-    <a class="navbar-brand btn-outline-success btn" href="#">my articles</a>
+    <a class="navbar-brand btn-outline-success btn" href="../otherPages/user_articles.php">my articles</a>
   </div>
 </nav>
